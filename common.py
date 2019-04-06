225e80a8a284c9d4d2896fcc3960336cf64cd10c
erv4gen <eremin.vladimir.d@gmail.com>
2019-04-06 17:35:54 -0500

    testing - new stage

---
+++ common.py
@@ -0,0 +1,75 @@
+
+import urllib , xmltodict , time 
+import pandas as pd
+import numpy as np
+import datetime as dt
+import matplotlib.pyplot as plt
+import warnings
+from matplotlib import animation
+import pandas_datareader.data as web
+from tqdm import tqdm
+
+#polinomial fitting
+
+def _fit_poly(tresdf):
+    x = np.array([-2.0,0.0,2.0])
+    xp = np.linspace(-3, 3, 100)
+    curve_params = pd.DataFrame()
+
+    curve_data = pd.DataFrame(index=xp)
+    print('Solving polynomials ...')
+    time.sleep(1)
+    for i in tqdm(range(len(tresdf))):
+        try:
+            y = np.array(tresdf.iloc[i])
+            z = np.polyfit(x, y, 3)
+            p = np.poly1d(z)
+            px = p(xp)
+            #_ = plt.plot(x, y, '.', xp, p(xp), '-')
+            #time.sleep(2)
+            #plt.ylim(-3,3)
+        except:
+            z = np.repeat(None,4)
+            px = np.repeat(None,100)
+        curve_data[tresdf.index[i].strftime('%Y-%m-%d')] = px
+        curve_params = curve_params.append(pd.Series(z,index=['pow3','pow2','pow1','c'],name=tresdf.index[i])
+                                              # ,ignore_index=True
+                                              )
+    curve_params.index = pd.to_datetime(curve_params.index)
+    curve_params = curve_params.dropna()
+    curve_data = curve_data.dropna(axis=1)
+    return curve_params , curve_data
+
+def plot_multi(data, cols=None,title='', spacing=.1,loc='upper center', **kwargs):
+
+    from pandas import plotting
+    plt.figure()
+    plt.title(title)
+    # Get default color style from pandas - can be changed to any other color list
+    if cols is None: cols = data.columns
+    if len(cols) == 0: return
+    colors = getattr(getattr(plotting, '_style'), '_get_standard_colors')(num_colors=len(cols))
+
+    # First axis
+    ax = data.loc[:, cols[0]].plot(label=cols[0], color=colors[0], **kwargs)
+    ax.set_xlabel("Days")
+    ax.set_ylabel(ylabel=cols[0])
+    lines, labels = ax.get_legend_handles_labels()
+
+    for n in range(1, len(cols)):
+        # Multiple y-axes
+        ax_new = ax.twinx()
+        ax_new.spines['right'].set_position(('axes', 1 + spacing * (n - 1)))
+        data.loc[:, cols[n]].plot(ax=ax_new, label=cols[n], color=colors[n % len(colors)])
+        ax_new.set_ylabel(ylabel=cols[n])
+
+        # Proper legend position
+        line, label = ax_new.get_legend_handles_labels()
+        lines += line
+        labels += label
+
+    ax.legend(lines, labels, loc=loc
+              #, bbox_to_anchor=(1, 0.5)
+             )
+    return ax
+
--- tsalib.py
+++ tsalib.py
@@ -1,5 +1,5 @@
 import os
-import sys , time , traceback , itertools , warnings , ipdb
+import sys , time , traceback , itertools , warnings , ipdb , uuid
 import datetime as dt
 
 from tqdm import tqdm
@@ -17,6 +17,34 @@ from arch import arch_model
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+
+warnings.filterwarnings('ignore')
+
+def get_data(date,ticker,data_source,horizon):
+    if data_source=='web':
+        start = '2000-01-01'
+        end = date
+        end_horizon =dt.datetime.strftime(dt.datetime.strptime(end,'%Y-%m-%d') + dt.timedelta(days=horizon)
+                             ,'%Y-%m-%d')
+
+        get_px = lambda x: web.DataReader(x, 'yahoo', start=start, end=end)
+
+        # symbols = ['SPY','TLT','MSFT']
+        # # raw adjusted close prices
+        # data = pd.DataFrame({sym:get_px(sym)['Adj Close'] for sym in symbols})
+
+        ticker_data = get_px(ticker)
+        ticker_data['vola'] = 100*(ticker_data.High - ticker_data.Low)/ticker_data['Adj Close']
+
+        ticker_data['rets'] = 100* ticker_data['Adj Close'].pct_change()
+        ticker_data = ticker_data.dropna()
+    else:
+        pass
+            #ticker_data = get_px(ticker)
+        # log returns
+        #lrets = np.log(ticker_data['Adj Close']/ticker_data['Adj Close'].shift(1)).dropna()
+        
+    return ticker_data
 
 
 def tsplot(y, lags=None, figsize=(8, 6), style='ggplot',title='Time Series Analysis Plots'):
@@ -89,7 +117,7 @@ def variance_quantiles(sim_paths,prc=0.75):
     sims_vars_np = np.array(sims_vars)
     return sim_paths[sims_vars.index( np.sort(sims_vars_np[(sims_vars_np > qa[1]) & (sims_vars_np < qa[2]) ])[0] )]
 
-def find_arch(TS,model,true_variance_forecast,horizon,print_model):
+def find_arch(TS,model,true_variance_forecast,horizon,print_model,method,dist):
 
     
     aic_d = []
@@ -99,7 +127,9 @@ def find_arch(TS,model,true_variance_forecast,horizon,print_model):
     params_factor_grid = list(itertools.product(*params_factor_list))
     print(f'Solving best {model} model ...')
     time.sleep(1)
+    
     for param in tqdm(params_factor_grid):
+        #ipdb.set_trace()
         p_ = param[0]
         q_ = param[1]
         o_ = 0
@@ -107,10 +137,10 @@ def find_arch(TS,model,true_variance_forecast,horizon,print_model):
         eval_aic = []
         eval_rmse = []
         for step in range(88,0,-22):
-            res = (arch_model(TS[:-step], p=p_, o=o_, q=q_, dist='StudentsT',vol='GARCH')
+            res = (arch_model(TS[:-step], p=p_, o=o_, q=q_, dist=dist,vol=model)
                                 .fit(update_freq=5, disp='off')
                               )
-            forecast  = (res.forecast(horizon=horizon, start=None, align='origin', method='simulation')
+            forecast  = (res.forecast(horizon=horizon, start=None, align='origin', method=method)
                     )
             expected_variance =  forecast.variance.iloc[-1].values
             eval_aic.append(res.aic)
@@ -120,8 +150,8 @@ def find_arch(TS,model,true_variance_forecast,horizon,print_model):
             else:
                 eval_rmse.append( np.sqrt(np.mean(np.power(expected_variance - true_variance_forecast[-step:(step*(-1)+horizon)],2) )) )
        
-        aic_d.append(np.product(np.log(eval_aic) ) )
-        RMSE.append(np.product(eval_rmse))
+        aic_d.append(np.mean(eval_aic ) )
+        RMSE.append(np.mean(eval_rmse ))
     combo_param = [a*b*1e3 for a,b in zip(aic_d ,RMSE)]
     best_params= params_factor_grid[(combo_param.index(min(combo_param)))]
     
@@ -142,31 +172,136 @@ def find_arch(TS,model,true_variance_forecast,horizon,print_model):
     print('best parameters: ',best_params, 'At index:',params_factor_grid.index(best_params))
     return params_factor_grid , best_params
 
-def vola_estimation(data,horizon,model,print_model,sim_prc):
-    TS = 100* data['Adj Close'].pct_change().dropna()
+def build_model(data,horizon,model,print_model,sim_prc,method,arch_params,mean_model,dist):
+    #TS = 100* data['Adj Close'].pct_change().dropna()
     #TS = rets[:-horizon]
     #predict_date = 
-    params_factor_grid , best_params = find_arch(TS=TS[:-horizon]
-                                                 ,model=model
+    if arch_params is None:
+        params_factor_grid , best_params = find_arch(TS=data.rets[:-horizon]
+                                                 ,mean=mean_model
                                                  ,true_variance_forecast=data.vola.values#[-horizon:].values
                                                  ,horizon=horizon
-                                                 ,print_model=print_model)
-    
-    res_final = (arch_model(TS, p=best_params[0], o=0, q=best_params[1], dist='StudentsT',vol=model)
+                                                 ,print_model=print_model
+                                                 ,model=model
+                                                 ,dist=dist
+                                                 ,method=method)
+    else:
+        best_params = arch_params
+    res_final = (arch_model(data.rets, p=best_params[0], o=0, q=best_params[1], dist=dist,vol=model)
                                 .fit(update_freq=5, disp='off')
             )
 
 
-    forecast  = (res_final.forecast(horizon=horizon, start=None, align='origin', method='simulation')
+    forecast  = (res_final.forecast(horizon=horizon, start=None, align='origin', method=method)
             )
-    
-    sims = forecast.simulations
+    if method=='simulation' or method=='bootstrap':
+        sims = forecast.simulations
 
-    simulated_path = sims.residual_variances[-1,:] #/100
+        #simulated_path_var = sims.residual_variances[-1,:] #/100
+        simulated_path_var = sims.values[-1,:]#.iloc[-1].values #/100
+        var_sim_ts = variance_quantiles(simulated_path_var,prc=sim_prc)
+        
+    else:
+        var_sim_ts = np.zeros(horizon)
+        
+    #ipdb.set_trace()
+    #expected_mean = forecast.mean.iloc[-1].values#.iloc[-1].values #/100
     expected_variance =  forecast.variance.iloc[-1].values #/100                              
+    
+    month_expect_var_d = np.sqrt(np.sum([np.power(x-var_sim_ts.mean(),2) for x in var_sim_ts]) *100/21)#/ data.iloc[-horizon,2]
+    month_expect_var = np.sqrt(np.sum([np.power(x-expected_variance.mean(),2) for x in expected_variance]) *100)
+#     month_expect_var =  np.sqrt( np.mean(
+#                                         #np.sum(
+#             [np.power(x-expected_mean,2) for x in expected_mean]
+#                                         #)      
+#                                         )
+#                                 )
+    
+    if print_model:
+        fig, ax = plt.subplots(1,1)
+        if method=='simulation' or method=='bootstrap':
+            lines = plt.plot(simulated_path_var[::30].T, color='#9cb2d6')
+            lines[0].set_label('Simulated path')
+        line = plt.plot(expected_variance, color='#002868')
+        line[0].set_label('Expected variance')
+        legend = plt.legend()
+        
+        f = res_final.hedgehog_plot(type='mean')
+        print(res_final)
 
-    most_var_sim_ts = variance_quantiles(simulated_path,prc=sim_prc)
     
-    month_expect_var = np.sum([np.power(x-most_var_sim_ts.mean(),2) for x in most_var_sim_ts]) *100/ data.iloc[-horizon,1]
+    return {'expected_var':month_expect_var,'expected_var_d':month_expect_var_d#, 'values':expected_mean
+           }
+
+def eval_model(TS,sim_prc,print_model,model,horizon,method,arch_params,mean_model,dist,ticker):
+    prev_month_vola =  np.sqrt( np.sum(
+            [np.power(x-TS.rets[-(horizon*2):-horizon].values.mean(),2) for x in TS.rets[-(2*horizon):-horizon].values]
+                                                            ) )
+
+    var_sim = build_model(data=TS.iloc[:-horizon]
+                                     ,horizon=horizon
+                                     ,model=model
+                                     ,print_model=print_model
+                                     ,sim_prc=sim_prc
+                                    ,method=method
+                                      ,dist=dist
+                                     ,arch_params=arch_params
+                                     ,mean_model=mean_model) 
+
+    real_vola =  np.sqrt( np.sum(
+            [np.power(x-TS.rets[-horizon:].values.mean(),2) for x in TS.rets[-horizon:].values]
+                                                            ) )
+    return {'model': 
+                    {
+                        'sim_id': str(uuid.uuid4())
+                        ,'ticker':ticker
+                        ,'arch_model':model
+                    ,'sim_prc':sim_prc
+                    ,'method':method
+                    ,'dist':dist
+                    ,'arch_params':arch_params
+                    ,'mean_model':mean_model
+                    ,'date': dt.datetime.strftime(TS.index[-1],'%Y-%m-%d')
+                        ,'prev_month_vola': round(prev_month_vola,2)
+                       # , 'expected_var':round(var_sim['expected_var'],2)
+                    , 'expected_var_d':round(var_sim['expected_var_d'],2)
+                       ,'real_vola':round(real_vola,2)
+                    }
+            ,'stats':
+                    {'null_rmse' : round( np.abs(prev_month_vola- real_vola) / prev_month_vola
+                                           ,2)
+                   #  ,'arch_rmse' : round(np.abs(var_sim['expected_var']- real_vola) / var_sim['expected_var'] ,2 )
+                     ,'arch_sim_rmse' : round(np.abs(var_sim['expected_var_d']- real_vola) / var_sim['expected_var_d'] ,2 )
+                    }
+           }
+
+
+def test_prediction(n_month_back,date,ticker,sim_prc,data_source,model,print_model,method,arch_params,mean_model,dist):
+    horizon=22
+    ticker_data = get_data(date,ticker,data_source,horizon=horizon)
+    res_dict = []
+    by_month_dict =[]
+    print('Running backtest ...')
+    time.sleep(1)
+    for step in tqdm(range(22*n_month_back,0,-22)):
+        res_dict.append(eval_model(TS=ticker_data.iloc[:-step]
+                                   ,ticker=ticker
+                                   ,sim_prc=sim_prc
+                                   ,print_model=print_model
+                                   ,model=model
+                                   ,horizon=horizon
+                                  ,method=method
+                                   ,dist=dist
+                                   ,mean_model=mean_model
+                                  ,arch_params=arch_params))
+        
+        #by_month_dict.append(res_dict)
+    null_error= round(sum(d['stats']['null_rmse'] for d in res_dict) / len(res_dict),2)
+   # arch_error = round(sum(d['stats']['arch_rmse'] for d in res_dict) / len(res_dict),2)
+    arch_d_error = round(sum(d['stats']['arch_sim_rmse'] for d in res_dict) / len(res_dict),2)
     
-    return {'expected_var':month_expect_var,'expected_var_ts':most_var_sim_ts}
+    return {'by_month':res_dict
+            ,'summary':{'null_rmse':null_error
+                     #   ,'arch_rmse':arch_error
+                        ,'arch_sim_rmse':arch_d_error
+                       }}
